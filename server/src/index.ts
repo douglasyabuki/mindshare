23763dcda2d31245064cfcc24759bffa64747754
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-11-28 08:20:39 -0300

    Add: JWT-based authentication middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import { expressMiddleware } from '@as-integrations/express5'
 import express from 'express'
 import 'reflect-metadata'
 import { buildSchema } from 'type-graphql'
+import { buildContext } from './graphql/context'
 import { AuthResolver } from './resolvers/auth.resolver'
 import { UserResolver } from './resolvers/user.resolver'
 
@@ -21,7 +22,13 @@ async function bootstrap() {
 
   await server.start()
 
-  app.use('/graphql', express.json(), expressMiddleware(server))
+  app.use(
+    '/graphql',
+    express.json(),
+    expressMiddleware(server, {
+      context: buildContext,
+    })
+  )
 
   app.listen(
     {
