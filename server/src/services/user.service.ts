94f6057c2aaac61498b72051e67ccff17c9f0a2d
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-12-04 07:30:12 -0300

    Add: User update service

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,5 +1,5 @@
 import { prismaClient } from '../../prisma/prisma'
-import { CreateUserInput } from '../dtos/input/user.input'
+import { CreateUserInput, UpdateUserInput } from '../dtos/input/user.input'
 
 export class UserService {
   async createUser(data: CreateUserInput) {
@@ -11,6 +11,25 @@ export class UserService {
     if (user) throw new Error('User already exists')
     return prismaClient.user.create({
       data,
+    })
+  }
+
+  async listUsers() {
+    return prismaClient.user.findMany()
+  }
+
+  async updateUser(id: string, data: UpdateUserInput) {
+    const user = await prismaClient.user.findUnique({
+      where: { id },
+    })
+    if (!user) throw new Error('User not found')
+
+    return prismaClient.user.update({
+      where: { id },
+      data: {
+        name: data.name ?? undefined,
+        role: data.role ?? undefined,
+      },
     })
   }
 
